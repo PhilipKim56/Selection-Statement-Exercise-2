d49b3041765b5dec15f8f7a22d5e8e09dc2f523e
Philip Kim <filshouse56@gmail.com>
2023-12-12 14:16:55 -0500

    Exercise Completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavoriteSubject();
+        }
+        public static void FavoriteSubject()
+        {
+            Console.WriteLine("What is your favorite subject?");
+            var userInput = (Console.ReadLine());
+
+            
+            switch (userInput) 
+            {
+                case "Math":
+                    Console.WriteLine($"{userInput}? Want to join the math club?");
+                    break;
+                case "English":
+                    Console.WriteLine($"{userInput}? What is your favorite book?");
+                    break;
+                case "Science":
+                    Console.WriteLine($"{userInput}? Have you ever done the mentos rocket?");
+                    break;
+                case "Gym":
+                    Console.WriteLine($"{userInput} nothing like a good sweat.");
+                    break;
+                case "Art":
+                    Console.WriteLine($"{userInput} really draws you in.");
+                    break;
+                default : Console.WriteLine($"{userInput}? How interesting...");
+                    break;
+            }
         }
     }
+    
 }
